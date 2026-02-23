cfe6689132847875c2ed8348b080332bc76583fa
faissalmaulana <114222238+faissalmaulana@users.noreply.github.com>
2026-02-23 09:25:01 +0700

    feat: add version flag

--- cmd/root.go
+++ cmd/root.go
@@ -4,9 +4,18 @@ Copyright © 2026 Faissal Maulana
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
+)
+
+const (
+	VERSION string = "0.0.0"
+)
+
+var (
+	version bool
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -18,6 +27,14 @@ var rootCmd = &cobra.Command{
 It will find common config files in $HOME and
 delete all associate config files of the package.
 	`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if version {
+			fmt.Println(VERSION)
+			return nil
+		}
+
+		return nil
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -34,6 +51,6 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cleaner.yaml)")
+	rootCmd.PersistentFlags().BoolVarP(&version, "version", "v", false, "installed cleaner version")
 
 }
